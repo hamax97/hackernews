0878c58a778b91718e4c759ad8a450dd2ac17aab
Hamilton Tobon Mosquera <htobonmm7@gmail.com>
2022-05-27 10:40:19 -0500

    fix: testing github action

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,5 @@ export const server = new ApolloServer({
 const port = process.env.PORT || 3000;
 
 server.listen({ port }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}!`);
+  console.log(`🚀 Server ready at ${url}`);
 });
